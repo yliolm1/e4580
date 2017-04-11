92c9b028cc694d7a2bce1944a89915ad458dab00
yliolm1 <matvej.yli-olli@aalto.fi>
2017-04-11 13:55:05 +0300

    Update abundant.cc

--- abundant.cc
+++ abundant.cc
@@ -2,24 +2,24 @@
 #include <cmath>
 #include <cstdlib>
 
-#include <errno.h>
+#include <errno.h> // we will include errnos, cmath, cstdlib due to strtol, square-rooting and error-checking
 using namespace std;
 
 int sum(int o){
 int altogether = 0;
-for (int n = 1; n <= sqrt(o); n++){
-if (o%n == 0){
-if (o/n != n){
+for (int n = 1; n <= sqrt(o); n++){ // this sum function takes a number and tries to trace smaller divisors
+if (o%n == 0){ // check that there won't be any abundance - should be always zero when looking for needed numbers
+if (o/n != n){ // check that we won't add the same number twice if division equals the given divisor
 
-altogether += n;
-altogether += (o/n);
-
-}
-else { altogether += n;}
+altogether += n; 
+altogether += (o/n); // add the small divisor and the result of the said division
 
 }
+else { altogether += n;} // just add the divisor if the requirement above is not fulfilled
+
 }
-altogether -= o;
+}
+altogether -= o; // we won't need the number in the sum - just its factors/divisors
 return altogether;
 
 
@@ -32,17 +32,17 @@ if (argc != 2){return -1;}
 char* p;
 errno = 0;
 int k = strtol(argv[1], &p, 10);
-if (*p != '\0' || errno != 0) return -1;
+if (*p != '\0' || errno != 0) return -1; //check that we have only 2 arguments, only int digits and nothing more
 
-int l = 0;
+int l = 0; // appearance counter given by the user
 
 
-for (int m = 1; m < 1000000; m++){
+for (int m = 1; m < 1000000; m++){ // 1000000 will be the overall maximum this program could reach
 
-if ((sum(m) > m)){
+if ((sum(m) > m)){ // see if the means of abundance are met - the number should be smaller than the sum of its divisors
 l++;
 cout << m << endl;
-if (l == k){return 0;}
+if (l == k){return 0;} // see if we match the appearance count given by the user
 }
 }
 return 0;
